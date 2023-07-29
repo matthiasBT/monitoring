426f4ddec34186748e07f7d3190f86af1de5b280
Temur Uzbekov <barkerhenry840@gmail.com>
2023-07-29 19:26:01 +0300

    Add server stub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+const addr = `:8080`
+
+func updateMetric(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc(`/update`, updateMetric)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
