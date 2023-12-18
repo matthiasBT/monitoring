1a3c9b48c30167f9ece7dae9cec110aa728ad845
Temur Uzbekov <barkerhenry840@gmail.com>
2023-12-18 22:15:07 +0200

    Add more tests

--- internal/agent/adapters/http_test.go
+++ internal/agent/adapters/http_test.go
@@ -4,19 +4,44 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
+	"io"
 	"testing"
 
+	"github.com/matthiasBT/monitoring/internal/infra/compression"
 	"github.com/matthiasBT/monitoring/internal/infra/logging"
 	"github.com/matthiasBT/monitoring/internal/infra/secure"
 	"github.com/matthiasBT/monitoring/internal/infra/utils"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_encryptData(t *testing.T) {
-	hmacKey := make([]byte, 32)
-	if _, err := rand.Read(hmacKey); err != nil {
-		t.Fatalf("Failed to create HMAC key: %v", err)
+func TestHTTPReportAdapter_compress(t *testing.T) {
+	reportAdapter := NewHTTPReportAdapter(
+		logging.SetupLogger(),
+		"0.0.0.0:8000",
+		"/updates/",
+		utils.Retrier{
+			Attempts:         1,
+			IntervalFirst:    0,
+			IntervalIncrease: 0,
+			Logger:           logging.SetupLogger(),
+		},
+		[]byte{},
+		nil,
+		0,
+	)
+	result, err := reportAdapter.compress([]byte("foobar"))
+	if err != nil {
+		t.Errorf("Failed to compress data: %v", err)
 	}
+
+	decompressed, err := compression.Decompress(io.NopCloser(bytes.NewReader(result.Bytes())))
+	if err != nil {
+		t.Errorf("Failed to decompress data: %v", decompressed)
+	}
+	assert.Equal(t, bytes.Equal(decompressed, []byte("foobar")), true)
+}
+
+func Test_encryptData(t *testing.T) {
 	privateKey, err := generateRSA()
 	if err != nil {
 		t.Fatalf("Failed to create RSA key")
@@ -31,11 +56,10 @@ func Test_encryptData(t *testing.T) {
 			IntervalIncrease: 0,
 			Logger:           logging.SetupLogger(),
 		},
-		hmacKey,
+		[]byte{},
 		&privateKey.PublicKey,
 		0,
 	)
-	reportAdapter.Jobs = make(chan []byte, 1)
 	cipher, err := reportAdapter.encryptData([]byte("foobar"))
 	if err != nil {
 		t.Errorf("Failed to encrypt data: %v", err)
