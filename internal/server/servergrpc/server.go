12dfc1ad05b5ded34583236ef1488c691cb15381
Temur Uzbekov <barkerhenry840@gmail.com>
2024-01-08 23:47:58 +0200

    Add grpc server GetMetric method

--- internal/server/servergrpc/server.go
+++ internal/server/servergrpc/server.go
@@ -2,18 +2,28 @@ package servergrpc
 
 import (
 	"context"
+	"errors"
 
+	common "github.com/matthiasBT/monitoring/internal/infra/entities"
 	"github.com/matthiasBT/monitoring/internal/infra/logging"
 	"github.com/matthiasBT/monitoring/internal/server/entities"
 	pb "github.com/matthiasBT/monitoring/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type Server struct {
 	pb.UnimplementedMonitoringServer
 	Logger  logging.ILogger
 	Storage entities.Storage
+}
+
+func NewServer(logger logging.ILogger, storage entities.Storage) *Server {
+	return &Server{
+		Logger:  logger,
+		Storage: storage,
+	}
 }
 
 func (s *Server) Ping(ctx context.Context, req *pb.Empty) (*pb.Empty, error) {
@@ -23,9 +33,52 @@ func (s *Server) Ping(ctx context.Context, req *pb.Empty) (*pb.Empty, error) {
 	return new(pb.Empty), nil
 }
 
-func NewServer(logger logging.ILogger, storage entities.Storage) *Server {
-	return &Server{
-		Logger:  logger,
-		Storage: storage,
+// GetMetric handles the GRPC request for retrieving a specific metric.
+// It validates the query, and writes the metric data back to the response.
+func (s *Server) GetMetric(ctx context.Context, req *pb.Metrics) (*pb.Metrics, error) {
+	metrics := common.Metrics{
+		ID:    req.Id,
+		MType: req.MType,
 	}
+	err := metrics.Validate(false)
+	if err != nil {
+		return nil, wrapInvalidMetricError(err)
+	}
+	result, err := s.Storage.Get(ctx, &metrics)
+	if err != nil {
+		var code codes.Code
+		if errors.Is(err, common.ErrUnknownMetric) {
+			code = codes.NotFound
+		} else {
+			code = codes.Internal
+		}
+		return nil, status.Errorf(code, err.Error())
+	}
+	return wrapMetrics(result), nil
+}
+
+func wrapInvalidMetricError(err error) error {
+	var code codes.Code
+	switch {
+	case errors.Is(err, common.ErrInvalidMetricType) || errors.Is(err, common.ErrInvalidMetricVal):
+		code = codes.InvalidArgument
+	case errors.Is(err, common.ErrMissingMetricName):
+		code = codes.NotFound
+	default:
+		code = codes.Internal
+	}
+	return status.Errorf(code, err.Error())
+}
+
+func wrapMetrics(result *common.Metrics) *pb.Metrics {
+	resp := new(pb.Metrics)
+	resp.Id = result.ID
+	resp.MType = result.MType
+	if result.Delta != nil {
+		resp.Delta = &wrapperspb.Int64Value{Value: *result.Delta}
+	}
+	if result.Value != nil {
+		resp.Value = &wrapperspb.DoubleValue{Value: *result.Value}
+	}
+	return resp
 }
